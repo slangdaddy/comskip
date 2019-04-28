66cbdd1e20281cd3d77ef095c4addd469140c6b1
John Mihalic <2854333+mezz64@users.noreply.github.com>
2019-04-28 04:26:13 +0000

    Fix python

--- start.sh
+++ start.sh
@@ -11,6 +11,6 @@ usermod -d /home nobody
 chown -R nobody:users /home
 
 # Start program
-python socket.py #$EMBY_API_KEY $EMBY_USER_KEY $EMBY_URL $EMBY_UNC
+python3 socket.py #$EMBY_API_KEY $EMBY_USER_KEY $EMBY_URL $EMBY_UNC
 
 echo "Stopping Container.."
